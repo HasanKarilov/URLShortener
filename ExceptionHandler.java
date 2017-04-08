dcf4c0553e423d8d23b9137b62c0f0b9ba928ded
HasanKarilov <hkarilov@gmail.com>
2017-04-08 18:47:38 +0600

    Helper & ExceptionHandler

---
+++ ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by hanaria on 4/8/17.
+ */
+public class ExceptionHandler {
+    public static void log(Exception e) {
+        Helper.printMessage(e.getMessage());
+    }
+}
