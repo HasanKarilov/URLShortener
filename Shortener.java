a6dec10a4f38ab1f31d072620ed67e4579d54b6c
HasanKarilov <hkarilov@gmail.com>
2017-04-08 18:30:49 +0600

    Заглушка методам

---
+++ Shortener.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by hanaria on 4/8/17.
+ */
+public class Shortener {
+
+
+
+    public synchronized Long getId(String string)
+    {
+        return null;
+    }
+
+
+    public synchronized String getString(Long id)
+    {
+        return null;
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by hanaria on 4/8/17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
